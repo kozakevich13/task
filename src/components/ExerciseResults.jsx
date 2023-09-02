b379a650078c35cb9a5c9272c8f9b0840af9816a
kozakevich13 <63108530+kozakevich13@users.noreply.github.com>
2023-09-02 16:34:18 +0300

    add material ui to ExerciseResults

--- src/components/ExerciseResults.jsx
+++ src/components/ExerciseResults.jsx
@@ -1,29 +1,46 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import Typography from "@mui/material/Typography";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
 
 const ExerciseResults = () => {
   const exerciseResults = useSelector((state) => state.exerciseResults) || [];
 
   return (
     <div>
-      <h2>Search Results:</h2>
+      <Typography variant="h5" component="h2">
+        Search Results:
+      </Typography>
       {exerciseResults.exerciseResults.length === 0 ? (
-        <p>there are no such exercises</p>
+        <Typography variant="body1">There are no such exercises</Typography>
       ) : (
-        <ul>
+        <List>
           {exerciseResults.exerciseResults.map((exercise, index) => (
-            <li key={index}>
-              <strong>name:</strong> {exercise.name}
-              <br />
-              <strong>type:</strong> {exercise.type}
-              <br />
-              <strong>muscle:</strong> {exercise.muscle}
-              <br />
-              <strong>instructions:</strong>
-              <p>{exercise.instructions}</p>
-            </li>
+            <ListItem key={index}>
+              <ListItemText
+                primary={
+                  <React.Fragment>
+                    <strong>name:</strong> {exercise.name}
+                  </React.Fragment>
+                }
+                secondary={
+                  <React.Fragment>
+                    <strong>type:</strong> {exercise.type}
+                    <br />
+                    <strong>muscle:</strong> {exercise.muscle}
+                    <br />
+                    <strong>instructions:</strong>
+                    <Typography variant="body2">
+                      {exercise.instructions}
+                    </Typography>
+                  </React.Fragment>
+                }
+              />
+            </ListItem>
           ))}
-        </ul>
+        </List>
       )}
     </div>
   );
